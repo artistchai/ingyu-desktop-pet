f479e27348aa4e025b5e9e069c774dae860734d4
권나현 <kwonnahyun@gwonnahyeon-ui-MacBookPro.local>
2026-08-19 14:16:50 +0900

    uiohook-napi 제거 및 Electron 31.6.0 버전 고정 (macOS 보안 오탐 문제 해결)

--- preload.js
+++ preload.js
@@ -4,11 +4,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 // 이렇게 뚫어서 노출시킵니다.
 contextBridge.exposeInMainWorld('electronAPI', {
   moveWindow: (dx, dy) => ipcRenderer.send('window-move', dx, dy),
-  // main.js가 uiohook으로 잡은 전역 마우스 위치를 렌더러(화면 코드)로 전달.
-  // 렌더러는 이 함수에 콜백만 등록하면 되고, ipcRenderer 자체는 못 만짐(보안).
-  onGlobalMouseMove: (callback) => {
-    ipcRenderer.on('global-mouse-move', (event, pos) => callback(pos));
-  },
 
   // ===== 뽀모도로 타이머 =====
   getTimerState: () => ipcRenderer.invoke('timer-get-state'),
